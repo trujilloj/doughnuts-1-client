10d6b586966a6f7303a068eef7890d4fd82b2641
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-11 10:30:54 -0600

    update main.js with new api url

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-let button = document.getElementById('button')
-let add = document.getElementById('append')
+let button = document.getElementById("button");
+let add = document.getElementById("append");
 
-button.addEventListener('click', (e) => {
-  add.innerText = ''
-  fetch('https://git.heroku.com/floating-lake-86488.git/cake/')
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((item) => {
-        let hele = document.createElement('h3')
-        hele.innerText = item.type
-        add.append(hele)
-      })
+button.addEventListener("click", e => {
+  add.innerText = "";
+  fetch("https://floating-lake-86488.herokuapp.com/cake")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(item => {
+        let hele = document.createElement("h3");
+        hele.innerText = item.type;
+        add.append(hele);
+      });
     })
-    .catch((err) => console.log(err))
-})
+    .catch(err => console.log(err));
+});
